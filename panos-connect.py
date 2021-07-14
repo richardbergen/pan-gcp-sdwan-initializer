f112a0e0f548c374efe071905be8b49ed2f488df
Richard Bergen <rbergen@analytikal.net>
2021-07-14 12:28:54 -0600

    used terraform count for student number tracking

--- panos-connect.py
+++ panos-connect.py
@@ -16,10 +16,13 @@ parser.add_argument("--change-password-to", help="Path to ssh private key file")
 parser.add_argument("--create-bootstrap", help="Creates bootstrap folder structure and necessary files for students")
 parser.add_argument("--panorama-serial-number", help="Serial number for Panorama registration")
 parser.add_argument("--create-api-key", help="Creates API key for PAN-OS device", action="store_true")
+parser.add_argument('--current-student-number', help=argparse.SUPPRESS)
 
 args = parser.parse_args()
 
 def main():
+
+    current_student_number = args.current_student_number
 
     panos_connection = None
 
@@ -61,42 +64,44 @@ def main():
 
     #if args.create_api_key and args.create_bootstrap:
     if args.create_bootstrap and 'ip' in locals():
-        student_state = {
-            'number_of_students_entered': 0,
-            'student_number_processed': 0,
-            'students_remaining': 0
-        }
+        #student_state = {
+        #    'number_of_students_entered': 0,
+        #    'student_number_processed': 0,
+        #    'students_remaining': 0
+        #}
 
         #if read_from_file(TMP_FILE):
-        if os.path.exists(TMP_FILE):
-            #number_of_students_remaining = int(read_from_file(TMP_FILE))
-            student_state_filedata = json.loads(read_from_file(TMP_FILE))
-            print('student_state_filedata ', student_state_filedata) ###
-            student_state['number_of_students_entered'] = student_state_filedata['number_of_students_entered'] ###
-            student_state['student_number_processed'] = student_state_filedata['student_number_processed'] ###
-
-            #student_number = number_of_students_remaining
-            print(f"Current student number: {student_state['student_number_processed']}")
-            #print(write_to_file(TMP_FILE, f'{number_of_students_remaining - 1}'))
-
-        else:
-            number_of_students = int(args.create_bootstrap)
-            print(f'No config found: number of students entered: {number_of_students}')
-
-            student_state['number_of_students_entered'] = number_of_students - 1 # counting from 0 ###
-
-            #print(write_to_file(TMP_FILE, f'{number_of_students - 1}'))
-            write_to_file(TMP_FILE, json.dumps(student_state)) ###
-
-            #student_number = number_of_students
-            print(f"Current student number being processed: {student_state['student_number_processed']}") ###
+        #if os.path.exists(TMP_FILE):
+        #    #number_of_students_remaining = int(read_from_file(TMP_FILE))
+        #    student_state_filedata = json.loads(read_from_file(TMP_FILE))
+        #    print('student_state_filedata ', student_state_filedata) ###
+        #    student_state['number_of_students_entered'] = student_state_filedata['number_of_students_entered'] ###
+        #    student_state['student_number_processed'] = student_state_filedata['student_number_processed'] ###
+#
+        #    #student_number = number_of_students_remaining
+        #    print(f"Current student number: {student_state['student_number_processed']}")
+        #    #print(write_to_file(TMP_FILE, f'{number_of_students_remaining - 1}'))
+#
+        #else:
+        #    number_of_students = int(args.create_bootstrap)
+        #    print(f'No config found: number of students entered: {number_of_students}')
+#
+        #    student_state['number_of_students_entered'] = number_of_students - 1 # counting from 0 ###
+#
+        #    #print(write_to_file(TMP_FILE, f'{number_of_students - 1}'))
+        #    write_to_file(TMP_FILE, json.dumps(student_state)) ###
+#
+        #    #student_number = number_of_students
+        #    #print(f"Current student number being processed: {student_state['student_number_processed']}") ###
+        #    print(f"Current student number being processed: current_student_number") ###
 
         #number_of_students_remaining = student_number - 1
         #print(f'number_of_students_remaining: {number_of_students_remaining}')
 
         panos_send_commands(panos_connection, command_type='configure', commands=[
             'set deviceconfig system timezone US/Pacific',
-            f"set deviceconfig system hostname Panorama-student-{student_state['student_number_processed']}",
+            #f"set deviceconfig system hostname Panorama-student-{student_state['student_number_processed']}",
+            f"set deviceconfig system hostname Panorama-student-{current_student_number}",
             'set deviceconfig system dns-setting servers primary 1.0.0.1',
             'set deviceconfig system ntp-servers primary-ntp-server ntp-server-address pool.ntp.org',
             'set template sdwan-template config vsys vsys1',
@@ -121,24 +126,24 @@ def main():
         #    sys.exit('ERROR: Bootstrap parameter entered was not a number. Please enter number of students to build the bootstrap for.')
         vm_auth_key = panos_create_vm_auth_key(ip, panos_api_key)
         #if number_of_students_remaining < 1:
-        print('before')
-        print(f"student_state['student_number_processed'] {student_state['student_number_processed']}")
-        print(f"student_state['number_of_students_entered'] {student_state['number_of_students_entered']}")
-        if student_state['student_number_processed'] > student_state['number_of_students_entered']:
-            print("student_state['student_number_processed'] > student_state['number_of_students_entered'], removing temp file")
-            #print('number_of_students_remaining < 1, removing file')
-            os.remove(TMP_FILE)
-        #create_bootstrap_terraform_files(student_number, vm_auth_key)
-        create_bootstrap_terraform_files(int(student_state['student_number_processed']), vm_auth_key)
+        #print('before')
+        #print(f"student_state['student_number_processed'] {student_state['student_number_processed']}")
+        #print(f"student_state['number_of_students_entered'] {student_state['number_of_students_entered']}")
+        #if student_state['student_number_processed'] > student_state['number_of_students_entered']:
+        #    print("student_state['student_number_processed'] > student_state['number_of_students_entered'], removing temp file")
+        #    #print('number_of_students_remaining < 1, removing file')
+        #    os.remove(TMP_FILE)
+        ##create_bootstrap_terraform_files(student_number, vm_auth_key)
+        create_bootstrap_terraform_files(current_student_number, vm_auth_key)
 
-        student_state['student_number_processed'] += 1 ###
-        print('after')
-        print(f"student_state['student_number_processed'] {student_state['student_number_processed']}")
-        print(f"student_state['number_of_students_entered'] {student_state['number_of_students_entered']}")
-        print('writing to file')
-        write_to_file(TMP_FILE, json.dumps(student_state)) ###d
-        print('Reading back from file')
-        print(json.loads(read_from_file(TMP_FILE)), '\n')
+        #student_state['student_number_processed'] += 1 ###
+        #print('after')
+        #print(f"student_state['student_number_processed'] {student_state['student_number_processed']}")
+        #print(f"student_state['number_of_students_entered'] {student_state['number_of_students_entered']}")
+        #print('writing to file')
+        #write_to_file(TMP_FILE, json.dumps(student_state)) ###d
+        #print('Reading back from file')
+        #print(json.loads(read_from_file(TMP_FILE)), '\n')
 
 
     if args.panorama_serial_number:
