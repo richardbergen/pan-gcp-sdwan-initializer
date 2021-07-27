0abd3ec605c4b0b0281f7fc8eb6e73d0e3af0d70
Richard Bergen <rbergen@analytikal.net>
2021-07-27 14:28:10 -0600

    split up Panorama commands

--- panos-connect.py
+++ panos-connect.py
@@ -97,14 +97,14 @@ def main():
         #number_of_students_remaining = student_number - 1
         #print(f'number_of_students_remaining: {number_of_students_remaining}')
 
-        panos_send_commands(panos_connection, command_type='configure', commands=[
+        panos_send_commands(panos_connection, command_type='configure', cmd_verify=False, commands=[
             'set deviceconfig system timezone US/Pacific',
             #f"set deviceconfig system hostname Panorama-student-{student_state['student_number_processed']}",
             f"set deviceconfig system hostname Panorama-student-{current_student_number}",
             'set deviceconfig system dns-setting servers primary 1.0.0.1',
             'set deviceconfig system ntp-servers primary-ntp-server ntp-server-address pool.ntp.org'])
         sleep(3)
-        panos_send_commands(panos_connection, command_type='configure', commands=[
+        panos_send_commands(panos_connection, command_type='configure', cmd_verify=False, commands=[
             'set template sdwan-template config vsys vsys1',
             'set template sdwan-template config  deviceconfig system ',
             'set template-stack sdwan-stack templates sdwan-template',
@@ -113,7 +113,7 @@ def main():
             'set template-stack sdwan-stack variable $wan2_ip type ip-netmask 1.1.1.2/32',
             'set template-stack sdwan-stack variable $lan_ip type ip-netmask 1.1.1.3/32'])
         sleep(3)
-        panos_send_commands(panos_connection, command_type='configure', commands=[
+        panos_send_commands(panos_connection, command_type='configure', cmd_verify=False, commands=[
             'set template-stack sdwan-stack config  vsys vsys1 zone Untrust network layer3',
             'set template-stack sdwan-stack config  vsys vsys1 zone Trust network layer3',
             'set template-stack sdwan-stack config  vsys vsys1 zone VPN network layer3',
